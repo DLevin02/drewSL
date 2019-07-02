42e8a9d3d69f2c4bdf8f5704eff6dcf6114eca50
DLevin02 <40250314+DLevin02@users.noreply.github.com>
2019-07-02 17:56:40 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Drew Levin.", "A Developer.", "A Freelancer."],
+      strings: ["Drew Levin.", "A Developer.", "A Problem Solver."],
       typeSpeed: 100,
       loop: true,
     });
